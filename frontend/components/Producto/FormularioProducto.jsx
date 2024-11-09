5d22272d4f97828bfae0a59ed4e112c697979ddb
W1TCH3R94 <jeduardo1394@gmail.com>
2024-11-09 17:01:10 -0600

    Actualización de frontend,  cambio del botón responsive pantalla completa del componente producto.

--- frontend/components/Producto/FormularioProducto.jsx
+++ frontend/components/Producto/FormularioProducto.jsx
@@ -3,44 +3,49 @@ import { useCallback } from "react";
 import Swal from "sweetalert2";
 import ModalCreateProducto from "./ModalGestionProducto";
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 
 const FormularioProducto = () => {
-    const [isModalOpen, setModalOpen] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const openCreateModal = () => {
-        setIsEditing(false);
-        setModalOpen(true);
-    };
+  const openCreateModal = () => {
+    setIsEditing(false);
+    setModalOpen(true);
+  };
 
-    const crearProducto = useCallback(() => {
-        Swal.fire({
-            icon: "success",
-            title: "Producto creado exitosamente",
-            showConfirmButton: false,
-            timer: 1000
-        });
-    }, []);
+  const crearProducto = useCallback(() => {
+    Swal.fire({
+      icon: "success",
+      title: "Producto creado exitosamente",
+      showConfirmButton: false,
+      timer: 1000,
+    });
+  }, []);
 
-    return (
-        <>
-            <Button 
-                onClick={openCreateModal} 
-                fullWidth={false} // Evita que ocupe el ancho completo
-                radius="full" 
-                size="lg" 
-                className="bg-principal text-white text-2xs"
-            >
-                Crear
-            </Button>
-            
-            <ModalCreateProducto
-                isOpen={isModalOpen}
-                onOpenChange={setModalOpen}
-                modo={isEditing}
-            />
-        </>
-    );
-}
+  return (
+    <>
+      <div className="flex justify-end">
+        <Button
+          onClick={openCreateModal}
+          radius="full"
+          size="lg"
+          className="bg-principal text-white text-2xs ml-auto mb-2"
+        >
+          Crear
+          <span>
+            <FaPlus />
+          </span>
+        </Button>
+
+        <ModalCreateProducto
+          isOpen={isModalOpen}
+          onOpenChange={setModalOpen}
+          modo={isEditing}
+        />
+      </div>
+    </>
+  );
+};
 
 export default FormularioProducto;
--- frontend/components/Producto/Producto.jsx
+++ frontend/components/Producto/Producto.jsx
@@ -4,23 +4,23 @@ import ModalProducto from "./ModalProducto";
 import { useDisclosure } from "@nextui-org/react";
 
 const Producto = () => {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    return (
-        <div className="w-full flex flex-col justify-center lg:gap-10 gap-4">
-            <div className="text-principal font-bold lg:text-3xl text-2xl flex justify-center mt-6">
-                <h1>Producto</h1>
-            </div>
-            <div className="w-full flex justify-end lg:w-auto mb-4"> {/* Ajustar el contenedor para alinear a la derecha */}
-                <FormularioProducto />
-            </div>
-            <div className="flex-col lg:flex-row w-full flex justify-center lg:gap-24 gap-4">
-                <div className="w-auto">
-                    <TablaProducto onOpen={onOpen} />
-                </div>
-            </div>
-            <ModalProducto isOpen={isOpen} onOpenChange={onOpenChange} />
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+    <div className="w-full flex flex-col justify-center lg:gap-10 gap-4">
+      <div className="text-principal font-bold lg:text-3xl text-2xl flex justify-center mt-6">
+        <h1>Producto</h1>
+      </div>
+      <div>
+        <div className="flex-col lg:flex-row w-full flex justify-center lg:gap-24 gap-4">
+          <div className="w-auto">
+            <FormularioProducto />
+            <TablaProducto onOpen={onOpen} />
+          </div>
         </div>
-    );
-}
+      </div>
+      <ModalProducto isOpen={isOpen} onOpenChange={onOpenChange} />
+    </div>
+  );
+};
 
 export default Producto;
